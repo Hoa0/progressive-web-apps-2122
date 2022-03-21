c3eab41af0549c3d4ad95f72d97fdea5ec5c66af
hoa0 <t-hoa.l@hotmail.com>
2022-03-21 23:37:32 +0100

    :poop: render ejs work

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express','4.17.3')
+const ejs = require('ejs');
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+/*
 app.get('/', (req, res) => {
   res.send('Hello World! h test nodemon hahahha yo')
-})
+})*/
+
+app.get('/', (req, res) => {
+  res.render('home',{
+    title:'List of all artcollection hi'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
