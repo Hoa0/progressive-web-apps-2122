aedceda06465f5a92396a3d035323589e1d08397
hoa0 <t-hoa.l@hotmail.com>
2022-03-28 11:09:58 +0200

    :poop: render template with data

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express','4.17.3')
-const ejs = require('ejs');
-const app = express()
-const port = 3000
 
-app.use(express.static('public'))
+//import fetch from 'node-fetch';
+//const express = require('express','4.17.3')
+const fetch =require('node-fetch')
+const express = require('express')
+const app = express()
+const dotenv = require("dotenv").config();
+const port = process.env.port || 3000;
+
+
+const baseURL = 'https://www.rijksmuseum.nl/api/nl/collection'
+const key = '?key=m37TFPjT&ps=20'
+const endpoint ='https://www.rijksmuseum.nl/api/nl/collection?key=m37TFPjT&ps=20'
+
+app.use(express.static('static'))
 app.set('view engine', 'ejs')
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World! h test nodemon hahahha yo')
-})*/
+
+const {
+    API_KEY
+} = process.env
 
 app.get('/', (req, res) => {
   res.render('home',{
@@ -16,6 +25,40 @@ app.get('/', (req, res) => {
   });
 });
 
+/*
+app.get('/overview', async(req, res) => {
+ const getData = await fetch(endpoint);
+ const json = await getData.json();
+console.log(json);
+
+  res.render('overview',{
+    title:'overzicht',
+  });
+});*/
+
+app.get('/overview', (req, res) => {
+    fetch(endpoint)
+        .then(async response => {
+            const artCollection = await response.json()
+            res.render('overview', {
+                title: 'overzicht iets',
+                data: artCollection.artObjects
+                
+            })
+           // console.log(artCollection);
+        })
+        .catch(err => res.send(err))
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/*
+async function getData(url){
+  return await fetch(url)
+  .then((response) => response.json())
+  .then((body)=> body.data)
+  .catch((error) => error)
+}*/
