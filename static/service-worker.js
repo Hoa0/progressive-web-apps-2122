cccba1f71af97c0f560680d606a1f7c6e4b5eb99
hoa0 <t-hoa.l@hotmail.com>
2022-04-03 21:51:54 +0200

    :wrench: add manifes

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,12 +1,16 @@
-const CORE_CACHE_VERSION = 'v3'
+const CORE_CACHE_VERSION = 'v4' // cache naam in de application
 const CORE_ASSETS = [
   '/offline',
   '/rijksmuseum.css',
 ]
 
+
 self.addEventListener('install', event => {
   console.log('Installing service worker')
 
+/**
+ * Het van een cache en voer het volgende opdracht uit
+ */
   event.waitUntil(
     caches.open(CORE_CACHE_VERSION).then(function(cache) {
       return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
